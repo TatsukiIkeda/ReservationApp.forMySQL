e7a3d356dafab0643e6dcd8f13c1dff86dbca33e
TatsukiIkeda <kisenan411@gmail.com>
2021-06-23 23:59:43 +0900

    コメント変更

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
+//今日の日付の一覧データを表示
     public function index(Request $request)
     {
         //ログインユーザ情報を取得
@@ -20,7 +21,7 @@ class OrderController extends Controller
         ->with(['items' => $items,'user' => $user]);
     }
 
-
+//選択した日付の一覧データを表示
     public function selectdate(Request $request)
     {
         //ログインユーザ情報を取得
@@ -34,6 +35,7 @@ class OrderController extends Controller
         ->with(['items' => $items,'date' => $date,'user' => $user]);
     }
 
+//新規登録画面を表示
     public function create(Request $request)
     {
         //ログインユーザ情報を取得
@@ -44,11 +46,12 @@ class OrderController extends Controller
         ->with(['items' => $items,'user' => $user]);
     }
 
+//入力した項目を新規レコードに保存
     public function store(Request $request)
     {
         //ログインユーザ情報を取得
         $user = Auth::user();
-        //入力したデータを変数$paramに入れる
+        //入力したデータを変数$orderに入れる
         $order = new Order();
         $order->order_name = $user->name;
         $order->arrival_date = $request->arrival_date;
@@ -62,18 +65,18 @@ class OrderController extends Controller
         return  redirect('order');
     }
 
+//選択したレコードを表示し編集する画面
     public function edit(Request $request, $order_id)
     {
         //ログインユーザ情報を取得
         $user = Auth::user();
         //受け取った$order_idから対象のレコードを呼び出す
         $item = Order::where('order_id', $order_id)->firstOrFail();
-        // $item = DB::table('ordres')->where('order_id', $order_id)->first();
         return view('order/edit')
         ->with(['item' => $item,'user' => $user,'order_id' => $order_id]);
     }
 
-//変数に代入したデータを指定したレコードに保存する
+//変更したデータを指定したレコードに保存する
     public function update(Request $request)
     {
         $order = Order::find($request->input('order_id'));//Order::find($request->$order_id);ではレコードを取得できない。
@@ -88,7 +91,7 @@ class OrderController extends Controller
         return redirect('order');
     }
 
-    //削除ボタンを押した時の処理
+//削除ボタンを押した時の処理
     public function del(Request $request, $order_id)
     {
         //受け取った$order_idから対象のレコードを呼び出す
@@ -97,33 +100,28 @@ class OrderController extends Controller
         return redirect('order');
     }
 
-    //入荷状況を変更する時の処理、入荷済みに変更
+//入荷状況を変更する時の処理、来店済みに変更
     public function instock(Request $request, $order_id)
     {
 
-        $order = Order::find($request->input('order_id'));//Order::find($request->$order_id);ではレコードを取得できない。
+        $order = Order::find($request->input('order_id'));
+        //flg=1未来店/flg=2来店済み
         $order->flg = '0';
         $order->save();
         //受け取った$order_idから対象レコードをflgデータを上書きする
         return redirect('order');
     }
 
-    //入荷状況を変更する時の処理、未入荷に変更
+//入荷状況を変更する時の処理、未来店に変更
     public function notinstock(Request $request, $order_id)
     {
         $order = Order::find($request->input('order_id'));
-        //変更するflgを変数$paramに入れる
+       //flg=1未来店/flg=2来店済み
         $order->flg = '1';
         //受け取った$order_idから対象レコードをflgデータを上書きする
         $order->save();
-
-
-
         return redirect('order');
     }
-
-
-
 
     public function logout()
     {
